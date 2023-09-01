6d9613e7a0211c1503a35d513cd25cc7eb220a23
otegaa <ekiugbotega@gmail.com>
2023-09-02 01:15:02 +0200

    Fix the close btn for shoe overview

--- src/components/ShoeOverview.js
+++ src/components/ShoeOverview.js
@@ -12,15 +12,17 @@ const ShoeOverview = ({
   return (
     <div className={styles.shoeOverviewOverlay}>
       <div className={styles.shoeOverviewContainer}>
-        <button className={styles.btnClose} onClick={onClose}>
-          <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-              fill="#fff"
-              fillRule="evenodd"
-            />
-          </svg>
-        </button>
+        <div className={styles.btnCloseContainer}>
+          <button className={styles.btnClose} onClick={onClose}>
+            <svg width="14" height="15" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                fill="#fff"
+                fillRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
         <div className={styles.shoeOverviewBigShoesContainer}>
           <div className={styles.shoeOverViewBigShoes}>
             <img
